89f64d4d1784b0dd468426004e39e1a6f2875050
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2021-03-27 16:34:25 +0900

    Update DeedRepository.sol
    コメントを追記しました。

--- backend/contracts/DeedRepository.sol
+++ backend/contracts/DeedRepository.sol
@@ -1,50 +1,54 @@
 pragma solidity ^0.5.16;
+// ERC721(ノンファンジルトークンのコントラクトを読み込む)
 import "./ERC721/ERC721Token.sol";
 
 /**
- * @title Repository of ERC721 Deeds
- * This contract contains the list of deeds registered by users.
- * This is a demo to show how tokens (deeds) can be minted and added 
- * to the repository.
+ * @title ERC721トークン(証書)のリポジトリコントラクト
+ * ユーザーが登録したトークン(証書)のリストを含む。
  */
 contract DeedRepository is ERC721Token {
 
+    /**
+    * トークンが登録された時のイベント
+    * @param _by 登録者のアドレス
+    * @param _tokenId トークンID
+    */
+    event DeedRegistered(address _by, uint256 _tokenId);
 
     /**
-    * @dev Created a DeedRepository with a name and symbol
-    * @param _name string represents the name of the repository
-    * @param _symbol string represents the symbol of the repository
+    * コンストラクター
+    * @param _name リポジトリの名前
+    * @param _symbol リポジトリのシンボル
     */
-    constructor(string memory _name, string memory _symbol) 
+    constructor(string memory _name, string memory _symbol) {
         public ERC721Token(_name, _symbol) {}
+    }
     
     /**
-    * @dev Public function to register a new deed
-    * @dev Call the ERC721Token minter
-    * @param _tokenId uint256 represents a specific deed
-    * @param _uri string containing metadata/uri
+    * 新しいトークン(証書)を登録する関数
+    * @dev ERC721トークン作成者が呼び出す
+    * @param _tokenId 特定のトークン(証書)を表すID
+    * @param _uri メタデータ/URI情報
     */
     function registerDeed(uint256 _tokenId, string memory _uri) public {
+        // トークン登録
         _mint(msg.sender, _tokenId);
+        // メタデータ追加
         addDeedMetadata(_tokenId, _uri);
+        // イベントの呼び出し
         emit DeedRegistered(msg.sender, _tokenId);
     }
 
     /**
-    * @dev Public function to add metadata to a deed
-    * @param _tokenId represents a specific deed
-    * @param _uri text which describes the characteristics of a given deed
-    * @return whether the deed metadata was added to the repository
+    * メタデータをトークン(証書)に追加するための関数
+    * @param _tokenId トークンID
+    * @param _uri トークンの特徴を記述するテキスト
+    * @return whether トークンのメタデータがリポジトリに追加されたかどうかを示す。
     */
     function addDeedMetadata(uint256 _tokenId, string memory _uri) public returns(bool){
+        // メタデータをセット
         _setTokenURI(_tokenId, _uri);
+        // 戻り値を返す。
         return true;
     }
-
-    /**
-    * @dev Event is triggered if deed/token is registered
-    * @param _by address of the registrar
-    * @param _tokenId uint256 represents a specific deed
-    */
-    event DeedRegistered(address _by, uint256 _tokenId);
 }
